18ef2e9f63fba549923d7b3e465db8ce54313e17
Rusin-Tomasz <rusin.tomasz90@gmail.com>
2018-03-23 08:55:12 +0100

    dodany smoothscroll

--- js/main.js
+++ js/main.js
@@ -1,24 +1,27 @@
-$(document).ready(function(){
+$(document).ready(function () {
     addBackgroundColor();
 });
 
-$(window).scroll(function(){
+$(window).scroll(function () {
     addBackgroundColor();
 })
 
 function addBackgroundColor() {
     var navHeight = parseInt($('#main-nav').outerHeight(), 10);
     var actualY = parseInt($(window).scrollTop(), 10);
-    
-    if(actualY >= navHeight) {
-       $('#main-nav').addClass('transparentBg'); 
-    }else {
-       $('#main-nav').removeClass('transparentBg'); 
-    }   
+
+    if (actualY >= navHeight) {
+        $('#main-nav').addClass('transparentBg');
+    } else {
+        $('#main-nav').removeClass('transparentBg');
+    }
 }
 
+//    -----Smooth Scroll------
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
 
-
-
-
-
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+});
